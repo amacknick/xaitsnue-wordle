bca75a8efa12b8b34f3c77cc32e81d96ea7de51c
amacknick <85263204+amacknick@users.noreply.github.com>
2024-06-05 15:36:31 -0700

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -3,9 +3,9 @@ export const CONFIG = {
   language: 'Xaitsnue', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
   author: 'Anna Macknick', // Put your name here so people know who made this game!
-  // authorWebsite: 'YourLinkHere', // Put a link to your website or social media here
+  authorWebsite: 'YourLinkHere', // Put a link to your website or social media here
   wordListSource: 'Robert Geary', // Describe the source material for your words here
- // wordListSourceLink: 'YourDictionaryLink', // Put a link to the source material for your words here
+  wordListSourceLink: 'YourDictionaryLink', // Put a link to the source material for your words here
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
   //
